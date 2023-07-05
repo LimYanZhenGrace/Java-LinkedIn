4d866da7f5634dd2fb1ee213fac7881c7870ce13
Lim Yan Zhen Grace <138642098+LimYanZhenGrace@users.noreply.github.com>
2023-07-05 07:21:47 +0000

    Reference Variables Answer done

--- _01_05b/MenuBuilder.java
+++ _01_05b/MenuBuilder.java
@@ -2,31 +2,42 @@ package _01_05b;
 
 import java.util.ArrayList;
 
-public class MenuBuilder {
-  public static void main(String[] args) {
-
+public class MenuBuilder 
+{
+  public static void main(String[] args) 
+  {
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
+    String menuTitle = "My Dream Menu";
 
     // Print the menuTitle variable to the console.
+    System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
+    ArrayList <String> menu = new ArrayList <String> ();
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
+    MenuItem starter = new MenuItem ("Garlic Edamame");
 
     // Add the starter variable to the ArrayList called menu.
+    menu.add(starter.toString());
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
+    MenuItem mainCourse = new MenuItem("Premium Wagyu Gyu");
 
     // Add the mainCourse variable to the ArrayList called menu.
+    menu.add(mainCourse.toString());
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
+    MenuItem dessert = new MenuItem("Matcha Zensai");
 
     // Add the dessert variable to the ArrayList called menu.
+    menu.add(dessert.toString());
 
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }

6ecb5570ae2982fdfdd5a6555a6f1ac73fb67eb2
Lim Yan Zhen Grace <138642098+LimYanZhenGrace@users.noreply.github.com>
2023-07-05 07:47:16 +0000

    Methods done

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -1,7 +1,6 @@
 package _02_04;
 
 public class Ticket {
-
   public Ticket() {
 
   }
@@ -12,8 +11,35 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setDestination(String destination) 
+  {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) 
+  {
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn) 
+  {
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public String getDestination() 
+  {
+    return destination;
+  }
 
+  public double getPrice() 
+  {
+    return price;
+  }
+
+  public boolean getIsReturn() 
+  {
+    return isReturn;
+  }
 }
